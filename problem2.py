9ec2a4fdfdeae906ae4db4361d6c68e33edd798c
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-06-30 09:05:29 +0530

    basic arithemetic problem

---
+++ problem2.py
@@ -0,0 +1,7 @@
+#Read two integers and print their sum, difference, and product.
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
