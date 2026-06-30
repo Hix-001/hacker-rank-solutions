28677391a38cd80e8e58924ceb1303feb3d499ac
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-06-30 09:39:07 +0530

    implemented large inetger arithmetic for power sum

---
+++ problem9.py
@@ -0,0 +1,7 @@
+#Read four integers a, b, c, d and print a^b + c^d.
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    print(a**b + c**d)
